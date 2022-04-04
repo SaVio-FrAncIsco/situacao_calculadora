4b3c1956cd98b49820bca6edfaf006d02cc24877
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-04-04 19:12:59 -0300

    update

--- assets/js/newscript.js
+++ assets/js/newscript.js
@@ -1,67 +1,101 @@
-let display= " "
-var num1= null
-var num2= null
-var resultado= null
+let display = "";
+var num1 = null;
+var num2 = null;
+var resultado = 0;
+let operador = '';
 
-let lebalDisplay=  document.getElementById("display")// altera o valor do display para o nº da fun escolhida
+let op = "";
+let lebalDisplay = document.getElementById("display");// altera o valor do display para o nº da fun escolhida
 
-function reset(){
-    let display= " "
-    var num1= null
-    var num2= null
-    var resultado= null
-    changeDisplay(0)
+function reset() {
+    display = "";
+    num1 = null;
+    num2 = null;
+    resultado = null;
+    soma = '';
+    changeDisplay(0);
 }
-function changeDisplay(valor){  //Função para alterar o valor mostrado no display da calculadora
-                                // altera a propriedade text da referência para o valor no display
-    lebalDisplay.innerHTML= valor
+function changeDisplay(valor) {  //Função para alterar o valor mostrado no display da calculadora
+    // altera a propriedade text da referência para o valor no display
+    lebalDisplay.innerHTML = valor;
 }
-function clickn0(){
-    display = display + "0" 
-    changeDisplay(display)
+function clickn0() {
+    display = display + "0";
+    changeDisplay(display);
 }
-function clickn1(){
+function clickn1() {
 
-    display = display + "1" 
-    changeDisplay(display)
+    display = display + "1";
+    changeDisplay(display);
 }
-function clickn2(){
+function clickn2() {
 
-    display = display + "2" 
-    changeDisplay(display)
+    display = display + "2";
+    changeDisplay(display);
 }
-function clickn3(){
+function clickn3() {
 
-    display = display + "3" 
-    changeDisplay(display)
+    display = display + "3";
+    changeDisplay(display);
 }
-function clickn4(){
+function clickn4() {
 
-    display = display + "4" 
-    changeDisplay(display)
+    display = display + "4";
+    changeDisplay(display);
 }
-function clickn5(){
+function clickn5() {
 
-    display = display + "5" 
-    changeDisplay(display)
+    display = display + "5";
+    changeDisplay(display);
 }
-function clickn6(){
+function clickn6() {
 
-    display = display + "6" 
-    changeDisplay(display)
+    display = display + "6";
+    changeDisplay(display);
 }
-function clickn7(){
+function clickn7() {
 
-    display = display + "7" 
-    changeDisplay(display)
+    display = display + "7";
+    changeDisplay(display);
 }
-function clickn8(){
+function clickn8() {
 
-    display = display + "8" 
-    changeDisplay(display)
+    display = display + "8";
+    changeDisplay(display);
 }
-function clickn9(){
+function clickn9() {
 
-    display = display + "9" 
-    changeDisplay(display)
+    display = display + "9";
+    changeDisplay(display);
+}
+function clickOpSom() {
+    op = "opSoma";
+    if(num1 === null && display !== ''){
+        soma = parseFloat(display);
+        if (isNaN(soma)) {
+            display = display + "+";
+            changeDisplay(display);
+        }
+        else {
+            num1 = soma + resultado;
+        }
+    }
+    
+    return resultado;
+}
+function lickOpSub() {
+    op = "opSub";
+}
+function clickOpDiv() {
+    op = "opDiv";
+}
+function clickOpMult() {
+    op = "opMult";
+}
+function igual() {
+    switch (op) {
+        case "opSoma":
+            changeDisplay(clickOpSom());
+            break;
+    }
 }
